4a557e5f397d2e762ff54706fad817e2892a6327
Ivan Yelizariev <yelizariev@it-projects.info>
2018-04-25 23:57:44 +0500

    [FIX] error on applying multi-theme with snippets on few websites
    closes #410
    
    [CI] test coverage for duplicated snippets

--- website_multi_theme/models/ir_ui_view.py
+++ website_multi_theme/models/ir_ui_view.py
@@ -45,3 +45,23 @@ class IrUiView(models.Model):
             return views
         current_website = request.website
         return views.filtered(lambda v: v.website_id == current_website)
+
+    # Workaround for https://github.com/odoo/odoo/pull/24429
+    def search(self, domain, offset=0, limit=None, order=None, count=False):
+        if self.env.context.get('search_multi_website_snippet'):
+            website_id = self.env.context['search_multi_website_snippet']
+            domain += [
+                '|',
+                ('website_id', '=', website_id),
+                ('website_id', '=', False)
+            ]
+            order = 'website_id DESC'
+            _logger.debug('Updated domain: %s', domain)
+
+        res = super(IrUiView, self).search(
+            domain, offset=offset, limit=limit, order=order, count=count)
+
+        if self.env.context.get('search_multi_website_snippet'):
+            return res[:1]
+        else:
+            return res
